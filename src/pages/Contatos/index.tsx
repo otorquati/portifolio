5399437988942835199d22f98658549957f204f6
Osvaldo <torquati.osvaldo@gmail.com>
2023-07-17 23:44:50 -0300

    Lesson five
    Included the content to page Contatos

--- src/pages/Contatos/index.tsx
+++ src/pages/Contatos/index.tsx
@@ -1,13 +1,39 @@
 import Container from '../../components/Container/Index';
 import Footer from '../../components/Footer';
 import Header from '../../components/Header';
+import styles from './Contatos.module.css';
+
+// Importação dos ícones da página
+import { GoMail } from 'react-icons/go';
+import { BsInstagram , BsYoutube, BsGithub, BsLinkedin} from 'react-icons/bs';
 
 function Contatos() {
   return ( 
     <>
     <Header />
     <Container>
+      <section className={styles.contatos}>
       <h2>Contatos</h2>
+      <h3>Entre em contato</h3>
+      <p>Para que possamos conversar mais sobre.</p>
+        <div className={styles.icones}>
+          <a href="mailto:torquati.osvaldo@gmail.com" target='_blank' rel='noopener noreferrer'>
+            <GoMail className={styles.icone} />
+          </a>
+          <a href="https://www.instagram.com/djthorkuat/" target='_blank' rel='noopener noreferrer'>
+            <BsInstagram className={styles.icone} />
+          </a>
+          <a href="https://www.youtube.com/channel/UCTeNEpZE2PcFleWXpia5_9w" target='_blank' rel='noopener noreferrer'>
+            <BsYoutube className={styles.icone} />
+          </a>
+          <a href="https://github.com/otorquati" target='_blank' rel='noopener noreferrer'>
+            <BsGithub className={styles.icone} />
+          </a>
+          <a href="https://www.linkedin.com/in/osvaldo-t-4a8b4439/" target='_blank' rel='noopener noreferrer'>
+            <BsLinkedin className={styles.icone} />
+          </a>
+        </div>
+      </section>
     </Container>
     <Footer />
     </>
