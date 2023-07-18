1f50720bd873b31759c498b2eb6324b293becfd9
Osvaldo <torquati.osvaldo@gmail.com>
2023-07-17 22:47:38 -0300

    Lesson four
    Crerating and adjusting Components:
    Sobre
    Projeto e
    Contato

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,14 @@
 import { Link } from 'react-router-dom'
 import Header from '../../components/Header';
 import styles from './Home.module.css'
+import Container from '../../components/Container/Index';
+import Footer from '../../components/Footer';
 
 function Home() {
   return ( 
     <>
       <Header />
+      <Container>
       <section className={styles.home}>
         <div  className={styles.apresentacao}>
           <p>
@@ -20,6 +23,8 @@ function Home() {
         <figure>
           <img className={styles.img_home} src="/developer-red.svg" alt="Imagem de Home" /></figure>
       </section>
+      </Container>
+      <Footer />
     </>
    );
 }
